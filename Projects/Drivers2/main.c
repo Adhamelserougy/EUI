852930926e637106a5bea8c3f78d717310f892fe
Adhamelserougy <adhamelserougy@gmail.com>
2023-08-28 02:53:19 +0300

    Update main.c

--- Projects/Drivers2/main.c
+++ Projects/Drivers2/main.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 //    Edge_Time_Capture();
-//    Stopwatch_Init();
+//    Stopwatch_Init();  /* Make sure to uncomment software delay and comment hardware delay in UTILS/delay.h or it won't work */
 //    Brightness_Control();
 //    Periodic_Delay();
 }
